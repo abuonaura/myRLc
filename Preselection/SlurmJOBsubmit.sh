1d9e0b56b160b0cb95e30cd29feac9ff83f8a38b
abuonaur <annarita.buonaura@cern.ch>
2021-08-19 10:34:28 +0200

    Added separate HLT1 and HLT2 option

--- Preselection/SlurmJOBsubmit.sh
+++ Preselection/SlurmJOBsubmit.sh
@@ -10,7 +10,7 @@ polarity=$2
 source  /home/hep/buonaura/conda_setup.sh
 conda activate lhcb_ana
 
-python /home/hep/buonaura/Analysis/RLc/scripts/Preselection/CreateTreePreselectionMC.py --MCTrackerOnly $1 $2 --presel
+python /home/hep/buonaura/Analysis/RLc/scripts/Preselection/CreateTreePreselectionMC.py --MCTrackerOnly $1 $2 --HLT1_1 --HLT1_2 --presel
 
 conda deactivate
 
